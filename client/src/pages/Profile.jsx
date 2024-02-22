72d1634ebeda791539b86709564f0505aee39eca
IT21754470 <it21754470@my.sliit.lk>
2024-02-22 12:40:56 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,8 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import {useDispatch} from 'react-redux';
+import { Link } from 'react-router-dom';
+
 
 export default function Profile() {
   const fileRef=useRef(null)
@@ -209,7 +211,7 @@ const handleSignOut = async () => {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
-
+<Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'to={"/create-listing"}>Create Listing</Link>
 
 
       </form>
